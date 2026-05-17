8d30b6d2ed7708feb2c9e38ba4ddda4237803cae
Eman Radwan <emanalaa11499@gmail.com>
2026-05-17 22:05:28 +0300

    feat(ui): add 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import notFoundImage from "../assets/images/404.png";
+import ButtonLink from "../components/ui/ButtonLink";
+import Container from "../components/ui/Container";
+import { MoveLeft } from "lucide-react";
 
-interface IProps {}
-
-const NotFound: React.FC<IProps> = ({}) => {
-  return <>NotFound</>;
+const NotFound: React.FC = () => {
+  return (
+    <section>
+      <Container>
+        <div className="flex flex-col items-center justify-center h-[85vh] lg:py-8">
+          <img
+            src={notFoundImage}
+            alt="not-found-image"
+            className="max-w-56 sm:max-w-72 md:max-w-96"
+          />
+          <ButtonLink to="/" className="lg:text-base!">
+            <MoveLeft />
+            Back to Home
+          </ButtonLink>
+        </div>
+      </Container>
+    </section>
+  );
 };
 
 export default NotFound;
